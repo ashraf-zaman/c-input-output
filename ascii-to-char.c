224d2d13582f073d27155ac5cb90546e92e5ecec
Ashraf <sm.ashraf.zaman>
2020-04-14 16:41:27 +0600

    Create ascii-to-char.c
    Convert ascii to character value in C

---
+++ ascii-to-char.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int r;
+    printf("Enter your any ASCII value : ");
+    scanf("%d",&r);
+    printf("Your ASCII character is : %c",r);
+
+    getch();
+    return 0;
+}
